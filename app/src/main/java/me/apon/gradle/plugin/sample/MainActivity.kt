71191d5d753e0905a27ae7ec4cf4143c7f3a2677
yaopeng <aponone@gmail.com>
2021-02-07 10:40:12 +0800

    for jitpack

--- app/src/main/java/me/apon/gradle/plugin/sample/MainActivity.kt
+++ app/src/main/java/me/apon/gradle/plugin/sample/MainActivity.kt
@@ -10,16 +10,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val dm = DisplayMetrics()
-        windowManager.defaultDisplay.getMetrics(dm)
-        val width = dm.widthPixels
-        val height = dm.heightPixels
-        val min = width.coerceAtMost(height)
-        val smallestWidth = min / (dm.densityDpi / 160.0)
-        assets
-        val v = resources.getDimension(R.dimen.base_swdp)
-        val tv = findViewById<TextView>(R.id.text)
-        tv.text =
-            "d700:" + v + "\nwidth:" + width + "\nheight:" + height + "\nmin:" + min + "\ndpi:" + dm.densityDpi + "\nsmallestWidth:" + smallestWidth + "\nbase:" + v / (dm.densityDpi / 160.0)
+//        val dm = DisplayMetrics()
+//        windowManager.defaultDisplay.getMetrics(dm)
+//        val width = dm.widthPixels
+//        val height = dm.heightPixels
+//        val min = width.coerceAtMost(height)
+//        val smallestWidth = min / (dm.densityDpi / 160.0)
+//        assets
+//        val v = resources.getDimension(R.dimen.base_swdp)
+//        val tv = findViewById<TextView>(R.id.text)
+//        tv.text =
+//            "d700:" + v + "\nwidth:" + width + "\nheight:" + height + "\nmin:" + min + "\ndpi:" + dm.densityDpi + "\nsmallestWidth:" + smallestWidth + "\nbase:" + v / (dm.densityDpi / 160.0)
     }
 }
